f4530ae8338db7cce12aba0b3ddbe1525565d43d
vincent-cailly <vincent.cailly@fr.ibm.com>
2020-01-30 18:32:05 +0100

    12th commit

--- tekton-pipeline/deploy.sh
+++ tekton-pipeline/deploy.sh
@@ -5,6 +5,7 @@ git commit -m "12th commit"
 git push
 oc apply -f pipeline/pipeline.yaml
 oc apply -f task/build-src-code.yaml 
+oc apply -f task/deploy-to-cluster.yaml 
 oc delete pr application-pipeline-run
 oc apply -f pipeline/pipeline-run.yaml
 
